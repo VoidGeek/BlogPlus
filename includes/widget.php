782c9b17cef6ff1f9cc6d810a5f4806b28bb1827
VoidGeek <pradyumna.p.6969@gmail.com>
2024-08-06 01:16:02 +0530

    Final for now

---
+++ includes/widget.php
@@ -0,0 +1,7 @@
+ <!-- Side Widget Well -->
+                <div class="well">
+                    <h4>Blogs</h4>
+                    <p>Explore the blogs here.Enjoy reading.........</p>
+                </div>
+
+            </div>
